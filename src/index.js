a531461ba15df130adf71eb6b839b9a563c430b2
Juan Rosario <pyroclasmx@yahoo.com>
2021-08-14 15:01:32 -0500

    Export tool class

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import GCodeViewer from './gcodeviewer'
 export default GCodeViewer;
 export { default as Axes } from './axes'
 export { default as BuildObjects } from './buildobjects'
-
+export { default as Tool } from './tool'
