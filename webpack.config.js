27fea3f9a80bce275a71edf301879143432ceb71
Juan Rosario <pyroclasmx@yahoo.com>
2021-08-14 19:06:43 -0500

    Tree shakable module

--- webpack.config.js
+++ webpack.config.js
@@ -7,21 +7,27 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'sindarius-gcodeviewer.js',
-    library: {
-      type: 'umd'
-    },
   },
   module: {
     rules: [
       {
         test: /\.(js)$/,
         exclude: /node_modules/,
-        use: ['babel-loader']
+        loader  : 'babel-loader',
+        options: {
+          presets: [
+            ['@babel/preset-env', { modules : false}]
+          ]
+        }
+
       }
     ]
   },
   resolve: {
     extensions: ['*', '.js']
+  },
+  optimization: {
+    usedExports: true,
   },
   externalsPresets: { node: true }, // in order to ignore built-in modules like path, fs, etc.
   externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
