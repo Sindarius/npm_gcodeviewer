b95e19feb987f9a7f083336fe2168c771688370d
Juan Rosario <pyroclasmx@yahoo.com>
2023-03-28 16:03:28 -0500

    Fix a Fanuc check

--- src/gcodeprocessor.js
+++ src/gcodeprocessor.js
@@ -696,7 +696,9 @@ export default class {
       let commands = tokenString.match(/[GM]+[0-9.]+/g); //|S+
 
       if (commands === null) {
-         let hasMove = tokenString.match(/[XYZ]+[0-9.]+/);
+         console.log(tokenString)
+         let hasMove = tokenString.match(/[XYZ]+[+-]?[0-9.]+/g);
+         console.log(hasMove);
          if (hasMove !== null) {
             commands = this.lastCommand;
          }
